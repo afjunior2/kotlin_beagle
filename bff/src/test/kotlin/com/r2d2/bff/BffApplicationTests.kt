ac05e86c3ef923b1be658068d4bedc4992f24ea9
Antonio Fernando da Silva Junior <antoniofernandodasilvajunior@gmail.com>
2022-02-21 22:56:35 -0300

    Commit inicial

---
+++ bff/src/test/kotlin/com/r2d2/bff/BffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.r2d2.bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
